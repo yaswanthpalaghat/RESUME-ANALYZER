46fa5e53a3c029283bcec32602dc2c0d2e4fb776
Yaswanth Sai Palaghat <yashu827284@gmail.com>
2017-10-06 14:26:32 +0530

    Add files via upload

---
+++ result.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<script>
+function forprint(){
+if (!window.print){
+ 
+return
+}
+window.print()
+}
+</script>
+</head>
+<body>
+<div style="width:1000px; border:2px #A514BF dashed; margin:0 auto; padding:10px;">
+	
+<h1 class="b"><strong>YOUR ANALYSIS REPORT</strong></h1>
+<h2>Desired job area:<?php echo $_POST["job"]; ?></h2>
+<h2>skill strength:good</h2>
+<h2>work experience and internshps:good</h2>
+<h2>projects:good</h2>
+<h2>achievements:good</h2>
+<h2>roles and responsibilities:</h2>
+<hr/>
+<h1>overview:you are amazing</h1>
+</div>
+<div align="center">
+				<a href="javascript:forprint()">
+					 <h1 font-style="algerian"><strong>Click here to Print the report</strong></h1></a>
+		</div>
+
+
+
+</body>
+</html>
